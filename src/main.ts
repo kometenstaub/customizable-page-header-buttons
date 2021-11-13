f0360f43207525e4c5763f51d225edc17c47d257
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-11-13 10:44:23 +0100

    refactor: :fire: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
 import { Plugin, setIcon, Platform, WorkspaceLeaf } from 'obsidian';
 
 export default class QuickSwitcherButtonPlugin extends Plugin {
-    // visited: Set<WorkspaceLeaf> = new Set();
 
-    addButton = (activeLeaf: Element, viewActions: Element) => {
+    addButton = (viewActions: Element) => {
         const quickSwitcherIcon = createEl('a', {
             cls: ['view-action', 'quick-switcher-button'],
         });
         setIcon(quickSwitcherIcon, 'go-to-file');
-        // const activeLeaf = document.getElementsByClassName(
-        //     'workspace-leaf mod-active'
-        // )[0];
-        // const viewActions =
-        //     activeLeaf.getElementsByClassName('view-actions')[0];
         viewActions.prepend(quickSwitcherIcon);
-        // console.log(viewActions);
 
         this.registerDomEvent(quickSwitcherIcon, 'click', () => {
             this.app.commands.executeCommandById('switcher:open');
@@ -42,13 +35,10 @@ export default class QuickSwitcherButtonPlugin extends Plugin {
         if (Platform.isMobile) {
             this.registerEvent(
                 this.app.workspace.on('file-open', () => {
-                    // console.log('file opened');
-                    // const currentLeaf = this.app.workspace.getLeaf();
 
                     const activeLeaf = document.getElementsByClassName(
                         'workspace-leaf mod-active'
                     )[0];
-                    // console.log(activeLeaf);
                     const viewActions =
                         activeLeaf.getElementsByClassName('view-actions')[0];
 
@@ -57,29 +47,8 @@ export default class QuickSwitcherButtonPlugin extends Plugin {
                             'view-action quick-switcher-button'
                         )[0]
                     ) {
-                        this.addButton(activeLeaf, viewActions);
+                        this.addButton(viewActions);
                     }
-                    // //@ts-expect-error
-                    // if (!this.visited.has(currentLeaf.id)) {
-                    //     // const activeLeaf = document.getElementsByClassName(
-                    //     //     'workspace-leaf mod-active'
-                    //     // )[0];
-                    //     // console.log(activeLeaf);
-                    //     // const viewActions =
-                    //     //     activeLeaf.getElementsByClassName(
-                    //     //         'view-actions'
-                    //     //     )[0];
-
-                    //     // if (
-                    //     //     !viewActions.getElementsByClassName(
-                    //     //         'view-action quick-switcher-button'
-                    //     //     )[0]
-                    //     // ) {
-                    //     //     this.addButton();
-                    //     // }
-                    // }
-                    // //@ts-expect-error
-                    // this.visited.add(currentLeaf.id);
                 })
             );
         }
