a2ba425f131910e8b45e92b9fa44b894ac678eb5
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-11-13 10:41:09 +0100

    refactor: :recycle: don't use pane history but instead check the DOM

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { Plugin, setIcon, Platform, WorkspaceLeaf } from 'obsidian';
 
-
 export default class QuickSwitcherButtonPlugin extends Plugin {
-    visited: Set<WorkspaceLeaf> = new Set();
+    // visited: Set<WorkspaceLeaf> = new Set();
 
-    addButton = () => {
+    addButton = (activeLeaf: Element, viewActions: Element) => {
         const quickSwitcherIcon = createEl('a', {
             cls: ['view-action', 'quick-switcher-button'],
         });
         setIcon(quickSwitcherIcon, 'go-to-file');
-        const activeLeaf = document.getElementsByClassName(
-            'workspace-leaf mod-active'
-        )[0];
-        const viewActions =
-            activeLeaf.getElementsByClassName('view-actions')[0];
+        // const activeLeaf = document.getElementsByClassName(
+        //     'workspace-leaf mod-active'
+        // )[0];
+        // const viewActions =
+        //     activeLeaf.getElementsByClassName('view-actions')[0];
         viewActions.prepend(quickSwitcherIcon);
+        // console.log(viewActions);
 
         this.registerDomEvent(quickSwitcherIcon, 'click', () => {
             this.app.commands.executeCommandById('switcher:open');
@@ -42,16 +42,46 @@ export default class QuickSwitcherButtonPlugin extends Plugin {
         if (Platform.isMobile) {
             this.registerEvent(
                 this.app.workspace.on('file-open', () => {
-                    const currentLeaf = this.app.workspace.getLeaf();
-                    //@ts-expect-error
-                    if (!this.visited.has(currentLeaf.id)) {
-                        this.addButton();
+                    // console.log('file opened');
+                    // const currentLeaf = this.app.workspace.getLeaf();
+
+                    const activeLeaf = document.getElementsByClassName(
+                        'workspace-leaf mod-active'
+                    )[0];
+                    // console.log(activeLeaf);
+                    const viewActions =
+                        activeLeaf.getElementsByClassName('view-actions')[0];
+
+                    if (
+                        !viewActions.getElementsByClassName(
+                            'view-action quick-switcher-button'
+                        )[0]
+                    ) {
+                        this.addButton(activeLeaf, viewActions);
                     }
-                    //@ts-expect-error
-                    this.visited.add(currentLeaf.id);
+                    // //@ts-expect-error
+                    // if (!this.visited.has(currentLeaf.id)) {
+                    //     // const activeLeaf = document.getElementsByClassName(
+                    //     //     'workspace-leaf mod-active'
+                    //     // )[0];
+                    //     // console.log(activeLeaf);
+                    //     // const viewActions =
+                    //     //     activeLeaf.getElementsByClassName(
+                    //     //         'view-actions'
+                    //     //     )[0];
+
+                    //     // if (
+                    //     //     !viewActions.getElementsByClassName(
+                    //     //         'view-action quick-switcher-button'
+                    //     //     )[0]
+                    //     // ) {
+                    //     //     this.addButton();
+                    //     // }
+                    // }
+                    // //@ts-expect-error
+                    // this.visited.add(currentLeaf.id);
                 })
             );
-
         }
     }
 
