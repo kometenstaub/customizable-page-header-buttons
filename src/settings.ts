4c9a320db1f9d814a5120cbc6c2679c0d84424a3
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-11-13 15:10:38 +0100

    refactor: :lipstick: add explanation to the settings

--- src/settings.ts
+++ src/settings.ts
@@ -20,44 +20,40 @@ export default class TopBarButtonsSettingTab extends PluginSettingTab {
             text: 'Top Bar Buttons Settings',
         });
 
+        containerEl.createEl('p', {
+            text: 'The buttons are added in the order you are enabling them.',
+        });
+
         for (const [id, obj] of Object.entries(idToNameAndIcon)) {
-            new Setting(containerEl)
-				.setName(obj.name)
-				.addToggle((toggle) => {
-                let buttonEnabled = false
-				if (settings.enabledButtons.includes(id)) {
-					buttonEnabled = true
-				}
+            new Setting(containerEl).setName(obj.name).addToggle((toggle) => {
+                let buttonEnabled = false;
+                if (settings.enabledButtons.includes(id)) {
+                    buttonEnabled = true;
+                }
                 if (buttonEnabled) {
-                    toggle
-						.setValue(true)
-						.onChange(async (state) => {
+                    toggle.setValue(true).onChange(async (state) => {
                         if (state) {
                             settings.enabledButtons.push(id);
                         } else {
                             settings.enabledButtons.remove(id);
-							this.plugin.removeButton(id)
+                            this.plugin.removeButton(id);
                         }
-						await this.plugin.saveSettings()
-						// console.log(this.plugin.settings.enabledButtons)
+                        await this.plugin.saveSettings();
+                        // console.log(this.plugin.settings.enabledButtons)
                     });
                 } else {
-                    toggle
-						.setValue(false)
-						.onChange(async (state) => {
+                    toggle.setValue(false).onChange(async (state) => {
                         if (state) {
                             settings.enabledButtons.push(id);
                         } else {
                             settings.enabledButtons.remove(id);
-							this.plugin.removeButton(id)
-                       	}
-						await this.plugin.saveSettings()
-						// console.log(this.plugin.settings.enabledButtons)
+                            this.plugin.removeButton(id);
+                        }
+                        await this.plugin.saveSettings();
+                        // console.log(this.plugin.settings.enabledButtons)
                     });
                 }
             });
         }
-
-
     }
 }
