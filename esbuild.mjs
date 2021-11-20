0984e63329187410ed4fee57f9b3819ff32f4b02
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-11-20 17:35:25 +0100

    chore: update build file

--- esbuild.mjs
+++ esbuild.mjs
@@ -30,7 +30,32 @@ This plugin uses feather icons (https://github.com/feathericons/feather). Feathe
 	SOFTWARE.
 
 
-The commandSuggester, iconPicker, addFeatherIcons function, feather icon names and the biggest part of the settings tab have been adapted from the Obsidian Customizable Sidebar Plugin (https://github.com/phibr0/obsidian-customizable-sidebar).
+The feather icons npm module uses itself the 'class names' npm package (https://github.com/JedWatson/classnames).
+It is MIT-licensed:
+	The MIT License (MIT)
+	
+	Copyright (c) 2018 Jed Watson
+	
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+	
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+	
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+
+The commandSuggester, iconPicker, addFeatherIcons function, Obsidian icon names and the biggest part of the settings tab have been adapted from the Obsidian Customizable Sidebar Plugin (https://github.com/phibr0/obsidian-customizable-sidebar).
 It is MIT-licensed:
 	MIT License
 
